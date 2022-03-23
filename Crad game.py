671eeac1f97a62118c9311c2f92b59852d76ff54
Sakshi <70653660+Sakshi887220@users.noreply.github.com>
2022-03-23 14:20:57 +0530

    Update Crad game.py

--- Crad game.py
+++ Crad game.py
@@ -26,11 +26,11 @@ playing = True
 class Card:
 
     def __init__(self,suit,rank):
-        self.suit = suit           # identity of a card
-        self.rank = rank
+       
+
         
     def __str__(self):
-        return self.rank + ' of ' + self.suit     #returns a string defining the card
+           #returns a string defining the card
 
 
 
@@ -38,40 +38,30 @@ class Card:
 class Deck:
     
     def __init__(self):
-        self.deck = []  # start with an empty list
-        for suit in suits:
-            for rank in ranks:
-                self.deck.append(Card(suit,rank))  # build Card objects and add them to the list
+          # build Card objects and add them to the list
     
     def __str__(self):
-        deck_comp = ''  # start with an empty string
-        for card in self.deck:
-            deck_comp += '\n '+card.__str__() # add each Card object's print string
-        return 'The deck has:' + deck_comp
+        # add each Card object's print string
+      
 
     def shuffle(self):           # Shuffles the 52 cards
-        random.shuffle(self.deck)          
+               
     def __len__(self):
-        return len(self.deck)
+       
 
 # Player class has all the information about the cards a player is holding
 class Player:
     def __init__(self,name):
-        self.cards = []
-        self.name = name
+       
     def add(self , new_cards):          # Add the cards from the table after the persor wins
-        self.cards.extend(new_cards)  
+       
         
     def move(self):                     #Puts one card on the table
-        return self.cards.pop(0)
-    
+      
     def __str__(self):
-        ans =''
-        for cards in self.cards:
-            ans += '\n' + cards.__str__()
-        return  self.name +" has the following cards:" +ans
+        
     def __len__(self):
-        return len(self.cards)
+       
    
 # this class has the information about all the cards on the table
 class Table:
@@ -132,30 +122,17 @@ while(True):
         print(user_1.name,"Please press enter to move a chance. You have a",card.__str__() ,'\n')
         a = input("Press any key")
         print("Top Cards on table are",'\n')
-        table.add(card)
-        print(table.cards[-1])
-        if len(table) > 1:
-            print(table.cards[-2])
-        if len(table) > 1:
-            if table.cards[-1].suit == table.cards[-2].suit or table.cards[-1].rank == table.cards[-2].rank:
-                print("Woohho Top two cards match" , user_1.name, "Wins this move",'\n')
-                user_1.add(table.cards[::-1])
-                table.remove()
+        
+
         player = 2
     else :
         card = user_2.move()
         print(user_2.name,"Please press enter to move a chance. You have a",card.__str__(),'\n')
         a = input("Press any key")
         print("Top Cards on table are",'\n')
-        table.add(card)
-        print(table.cards[-1])
-        if len(table) > 1:
-            print(table.cards[-2])
-        if len(table) > 1:
-            if table.cards[-1].suit == table.cards[-2].suit or table.cards[-1].rank == table.cards[-2].rank:
-                print("Woohho Top two cards match" , user_2.name, "Wins this move",'\n')
-                user_2.add(table.cards[::-1])
-                table.remove()
+       
+
+
         player = 1
         
     print('\n')
